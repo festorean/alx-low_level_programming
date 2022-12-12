2d2f34b6f9ac37f18b81d457defbad8cce7ca409
Adebayo Festus <festry124@gmail .com>
2022-12-11 16:49:05 -0800

    Glory to God

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,8 @@ int main(void)
 
 	printf("size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
 	printf("size of a int: %lu bytes(S)\n", (unsigned long)sizeof(b));
-	printf("size of a long: %lu bytes(S)\n", (unsigned long)sizeof(c));
+	printf("size of a long int: %lu bytes(S)\n", (unsigned long)sizeof(c));
 	printf("size of a long long: %lu bytes(S)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %lu bytes(S)\n", (unsigned long)sizeof(f));
 	return (0);
+}
