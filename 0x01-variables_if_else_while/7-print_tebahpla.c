a9b4c586b81cf8b15dd81d0429777075edb9110b
Adebayo Festus <festry124@gmail .com>
2022-12-13 08:23:58 -0800

    lowercase backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print lowercase in reverse
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
