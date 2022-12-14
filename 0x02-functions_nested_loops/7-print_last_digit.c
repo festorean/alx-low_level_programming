cd169a5d3b6eced3bdaccaa7370b153b21196acb
Adebayo Festus <festry124@gmail .com>
2022-12-14 15:03:11 -0800

    print last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,9 @@
 */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int ld;
+
+	ld = n % 10;
 
 	if (ld < 0)
 	{
@@ -15,5 +17,5 @@ int print_last_digit(int n)
 	}
 	_putchar(ld + '0');
 
-	return (0);
+	return (ld);
 }
