8d2af00195a9ed5262e570f51b8a75063bfec7e8
Adebayo Festus <festry124@gmail .com>
2022-12-14 00:34:43 -0800

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print putchar
+ * code written by me
+ * Return: 0
+*/
+
+int main(void)
+{
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
