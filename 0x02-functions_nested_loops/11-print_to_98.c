0023eceb159dfdb1e1e65894ef6d1f3ecc906835
Adebayo Festus <festry124@gmail .com>
2022-12-14 17:59:03 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,17 +8,17 @@
 
 void print_to_98(int n)
 {
-while (n < 98)
-{
-	printf("%i, ", n};
-	n++;
-}
-while (n > 98)
-{
-	printf("%i, ", n);
-	n--;
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
 
-printf("98");
-putchar('\n');
-}
